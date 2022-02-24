89a4497673a116dcdb3bdd39c4210fffd8c567f6
first_name <ss329222@gmail.com>
2022-02-24 07:03:42 +0530

    Added:EditContacrDetails

--- src/com/blz/day10practice/AddressBook.java
+++ src/com/blz/day10practice/AddressBook.java
@@ -1,6 +1,7 @@
 package com.blz.day10practice;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class AddressBook {
@@ -12,15 +13,14 @@ public class AddressBook {
 			try (Scanner sc1 = new Scanner(System.in)) {
 				int choice;
 				do {
-					System.out.println("1.INSERT");
-					System.out.println("2.DISPLAY");
-					System.out.println("Enter your choice");
+					System.out.println("1.INSERT" + "\n2.DISPLAY" + "\n3.EDIT");
+					System.out.println("Enter your 1st choice");
 					choice = sc.nextInt();
 
 					switch (choice) {
 					case 1:
 						System.out.print("Enter First Name: ");
-						String firstName = sc1.nextLine();
+						String firstName1 = sc1.nextLine();
 						System.out.print("Enter last Name: ");
 						String lastName = sc1.nextLine();
 						System.out.print("Enter your address: ");
@@ -35,16 +35,66 @@ public class AddressBook {
 						String phoneNo = sc1.nextLine();
 						System.out.print("Enter your email: ");
 						String email = sc1.nextLine();
-						Contacts contact = new Contacts(firstName, lastName, address, city, state, zipcode, phoneNo,
+						Contacts contact = new Contacts(firstName1, lastName, address, city, state, zipcode, phoneNo,
 								email);
 						array.add(contact);
 						break;
 					case 2:
 						System.out.println(array);
 						break;
+					case 3:
+						boolean found = false;
+						System.out.println("Enter first name of the person for update ");
+						firstName1 = sc1.nextLine();
+						System.out.println("------------------------------------");
+
+						ListIterator<Contacts> listIterator = array.listIterator();
+						/*
+						 * It is a java iterator which is used to traverse all types of lists including
+						 * ArrayList, Vector, LinkedList, Stack
+						 */
+
+						while (listIterator.hasNext()) {
+							contact = listIterator.next();
+							// condition if firstname matches
+							if (contact.getFirstName().equals(firstName1)) {
+
+								System.out.print("Enter new first Name: ");
+								firstName1 = sc1.nextLine();
+								System.out.print("Enter new last Name: ");
+								lastName = sc1.nextLine();
+								System.out.print("Enter your new address: ");
+								address = sc1.nextLine();
+								System.out.print("Enter your new city: ");
+								city = sc1.nextLine();
+								System.out.print("Enter your state: ");
+								state = sc1.nextLine();
+								System.out.print("Enter zip code : ");
+								zipcode = sc.nextInt();
+								System.out.print("Enter phone number: ");
+								phoneNo = sc1.nextLine();
+								System.out.print("Enter your eMail ID: ");
+								email = sc1.nextLine();
+								listIterator.set(new Contacts(firstName1, lastName, address, city, state, zipcode,
+										phoneNo, email));
+								found = true;
+							}
+						}
+						System.out.println("_____________________________________");
+
+						if (!found) {
+							System.out.println("Record not found");// o/p for mismatch first name
+						} else {
+							System.out.println("Record is updated successfully");
+						}
+						System.out.println("_______________________________________");
+						break;
+					default:
+						System.out.println("default");
+
 					}
 				} while (choice != 0);
-				System.out.println("Invalid Choice!!!");
+				System.out.println("Invalid Choice!!");
 			}
 		}
 	}
